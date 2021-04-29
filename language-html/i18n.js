7cb150f5ab53f78f52d367d744f887a4155a07bf
yueshun lu <yueshun.lu@qiyuesuo.com>
2021-04-29 14:07:48 +0800

    convert preCondition to interceptor

--- language-html/i18n.js
+++ language-html/i18n.js
@@ -2,27 +2,16 @@ const { convertFilter } = require('./utils/convert-filter');
 const { isObjectString } = require('./utils/is-object-string');
 const { wrapWithTemplateLiteral } = require('./utils/wrap-with-template-literal');
 const { isVForOf, vForOfPreprocess, vForOfPostprocess } = require('./utils/v-for-of');
-var languageJs = require('../language-js');
+const languageJs = require('../language-js');
 
-const hasChinese = /[\u4e00-\u9fa5]/;
 function textI18n(node) {
-  if (!hasChinese.test(node.value)) {
-    return;
-  }
   node.value = node.value.replace(node.value.trim(), `{{${languageJs(node.interpolationText).slice(0, -1)}}}`);
 }
 function attrI18n(node) {
-  if (!hasChinese.test(node.value)) {
-    return;
-  }
   node.value = languageJs(wrapWithTemplateLiteral([node.value])).slice(0, -1);
   node.name = `:${node.name}`;
 }
 function dirI18n(node) {
-  if (!hasChinese.test(node.value)) {
-    return;
-  }
-
   const { value } = node;
   try {
     if (isObjectString(value)) {
@@ -52,7 +41,7 @@ function dirI18n(node) {
 
 function travserNode(node) {
   const handler = {
-    text: textI18n,
+    text: hasChinese(textI18n),
     element: (node) => {
       node.attrs.forEach((attr) => {
         travserNode(attr);
@@ -61,8 +50,8 @@ function travserNode(node) {
         travserNode(node);
       });
     },
-    attribute: attrI18n,
-    directive: dirI18n,
+    attribute: hasChinese(attrI18n),
+    directive: hasChinese(dirI18n),
     comment: (node) => node,
   };
   handler[node.type](node);
@@ -76,6 +65,7 @@ function travserFunctionalNode(functionalNode) {
   functionalNode.map((node) => {
     const { value: nodeValue } = node;
     // todo better using languageJs() instead of regular expression
+    const hasChinese = /[\u4e00-\u9fa5]/;
     if (hasChinese.test(nodeValue) && /\$t/.test(nodeValue) && !/parent\.\$t/.test(nodeValue)) {
       node.value = nodeValue.replace(/\$t/, 'parent.$t');
     }
@@ -86,3 +76,11 @@ module.exports = function init(node) {
   travserNode(node);
   travserFunctionalNode(node);
 };
+function hasChinese(fn) {
+  return function (node) {
+    if (!/[\u4e00-\u9fa5]/.test(node.value)) {
+      return;
+    }
+    fn(node);
+  };
+}
